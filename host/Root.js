87a38b15ee5f5897d6684df41dfcbdf83c627b6e
andres.f.zuluaga <andres.zuluaga@wolox.co>
2022-01-19 10:35:23 -0500

    Add .env files

--- host/Root.js
+++ host/Root.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { ChunkManager } from '@callstack/repack/client';
+import Config from "react-native-config";
 
 import { AppNavigator, AuthNavigator } from './src/app/components/AppNavigator';
 
@@ -9,27 +10,27 @@ ChunkManager.configure({
   forceRemoteChunkResolution: true,
   resolveRemoteChunk: async (chunkId, parentId) => {
     let url;
-
+    console.log(Config.LOCAL_IP);
     switch (parentId) {
       case 'wbooks':
-        url = `http://localhost:9001/${chunkId}.chunk.bundle`
+        url = `http://${Config.LOCAL_IP}:9001/${chunkId}.chunk.bundle`
         break;
       case 'app3':
-        url = `http://localhost:9002/${chunkId}.chunk.bundle`
+        url = `http://${Config.LOCAL_IP}:9002/${chunkId}.chunk.bundle`
         break;
       // case 'home':
       //   url = `http://localhost:9002/${chunkId}.chunk.bundle`
       //   break;
       case 'login':
-        url = `http://localhost:9004/${chunkId}.chunk.bundle`
+        url = `http://${Config.LOCAL_IP}:9004/${chunkId}.chunk.bundle`
         break;
       case 'main':
       default:
         url = {
-          app3: 'http://localhost:9002/app3.container.bundle',
+          app3: `http://${Config.LOCAL_IP}:9002/app3.container.bundle`,
           // home: 'http://localhost:9002/app3.container.bundle',
-          login: 'http://localhost:9004/login.container.bundle',
-        }[chunkId] ?? `http://localhost:8081/${chunkId}.chunk.bundle`
+          login: `http://${Config.LOCAL_IP}:9004/login.container.bundle`,
+        }[chunkId] ?? `http://${Config.LOCAL_IP}:8081/${chunkId}.chunk.bundle`
         break;
     }
 
